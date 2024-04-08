e3c5c6d1eba68fb9320a0d1d16412512cfe4f9aa
Niklas Pelli <niklaspelli22@gmail.com>
2024-04-08 18:39:29 +0200

    fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { Container, Col } from "react-bootstrap";
+import { Container, Col, Row } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faReact,
@@ -7,6 +7,8 @@ import {
   faNodeJs,
   faHtml5,
 } from "@fortawesome/free-brands-svg-icons";
+
+import { styled } from "styled-components";
 
 export default function Home() {
   return (
@@ -17,53 +19,67 @@ export default function Home() {
       <div className="typer">
         <div className="glow">Frontend Developer...</div>
       </div>
-      <Col>
-        <FontAwesomeIcon
-          icon={faReact}
-          style={{
-            color: "orange",
-            fontSize: "5rem",
-            margin: "10px",
-            marginBottom: "20px",
-          }}
-        />
-        <FontAwesomeIcon
-          icon={faCss3Alt}
-          style={{
-            color: "orange",
-            fontSize: "5rem",
-            margin: "10px",
-            marginBottom: "20px",
-          }}
-        />
-        <FontAwesomeIcon
-          icon={faJs}
-          style={{
-            color: "orange",
-            fontSize: "4rem",
-            margin: "10px",
-            marginBottom: "20px",
-          }}
-        />
-        <FontAwesomeIcon
-          icon={faNodeJs}
-          style={{
-            color: "orange",
-            fontSize: "4rem",
-            margin: "10px",
-            marginBottom: "20px",
-          }}
-        />
-        <FontAwesomeIcon
-          icon={faHtml5}
-          style={{
-            color: "orange",
-            fontSize: "4rem",
-            margin: "10px",
-            marginBottom: "20px",
-          }}
-        />
-      </Col>
+      <Row className="justify-content-center align-items-center h-100">
+        <Col>
+          <Card>
+            <div className="skills">
+              <FontAwesomeIcon
+                icon={faReact}
+                style={{
+                  color: "cyan",
+                  fontSize: "3.5rem",
+                  margin: "10",
+                }}
+              />
+              <FontAwesomeIcon
+                icon={faCss3Alt}
+                style={{
+                  color: "blue",
+                  fontSize: "3.5rem",
+                  margin: "10",
+                }}
+              />
+
+              <FontAwesomeIcon
+                icon={faJs}
+                style={{
+                  color: "yellow",
+                  fontSize: "3.5rem",
+                  margin: "10",
+                }}
+              />
+              <FontAwesomeIcon
+                icon={faNodeJs}
+                style={{
+                  color: "green",
+                  fontSize: "3.5rem",
+                  margin: "10",
+                }}
+              />
+              <FontAwesomeIcon
+                icon={faHtml5}
+                style={{
+                  color: "orange",
+                  fontSize: "3.5rem",
+                  margin: "10",
+                }}
+              />
+            </div>
+          </Card>
+        </Col>
+      </Row>
     </Container>
   );
 }
+
+const Card = styled.div`
+  .skills {
+    display: flex;
+    position: relative;
+    top: -50px;
+    place-content: center;
+    margin: 1rem;
+    opacity: 50%;
+  }
+  }
+`;
