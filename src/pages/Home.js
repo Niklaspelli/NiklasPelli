5b591ba6400f752e040ca5ff46213bce2c061538
Niklas Pelli <niklaspelli22@gmail.com>
2024-04-27 22:25:01 +0200

    fixat parametrar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import { Container, Col, Row } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { motion } from "framer-motion";
+import CountUp from 'react-countup';
 import {
   faReact,
   faCss3Alt,
@@ -8,7 +9,7 @@ import {
   faNodeJs,
   faHtml5,
 } from "@fortawesome/free-brands-svg-icons";
-
+import "../styles.css";
 import { styled } from "styled-components";
 
 export default function Home() {
@@ -22,6 +23,9 @@ export default function Home() {
           <div className="typer">
             <div className="glow">Frontend Developer...</div>
           </div>
+          
+           </Col>
+ </Row>
 
           <Card
             animate={{ opacity: 1 }}
@@ -29,52 +33,173 @@ export default function Home() {
             exit={{ opacity: 1 }}
             transition={{ duration: 15 }}
           >
+            
+          
             <div className="skills">
-              <FontAwesomeIcon
+           
+              <Row className="justify-content-center">
+                
+              <Col md={2} sm={7}> 
+               
+                
+                 
+  <div className="circle-wrap">
+  <div className="circle">
+    <div className="mask full-1">
+      <div className="fill-1"></div>
+    </div>
+    <div className="mask half">
+      <div class="fill-1">
+      </div>
+      
+    </div>
+    
+<div className="inside-circle">
+  <FontAwesomeIcon
                 icon={faReact}
                 style={{
                   color: "cyan",
-
-                  margin: "10",
+                  fontSize: "5rem",
+                  margin: "20px"
+                
                 }}
               />
-              <FontAwesomeIcon
+              <span className="progress-value">
+              <CountUp start={0} 
+              end={40}
+              duration={4.75}></CountUp>%</span> 
+  </div> 
+  </div>
+</div>
+
+            
+</Col>
+<Col md={2} sm={7}>
+ 
+  <div className="circle-wrap">
+  <div className="circle">
+    <div className="mask full-2">
+      <div className="fill-2"></div>
+    </div>
+    <div className="mask half">
+      <div class="fill-2">
+      </div>
+      
+    </div>
+<div className="inside-circle2">
+  <FontAwesomeIcon
                 icon={faCss3Alt}
                 style={{
                   color: "blue",
-
-                  margin: "10",
+                  fontSize: "5rem",
+                  margin: "20px"
                 }}
               />
+              <span className="progress-value">
+              <CountUp start={0}
+               end={60}
+               duration={7.75}></CountUp>%</span>
+</div>
+  </div>
+</div>
 
-              <FontAwesomeIcon
+</Col> 
+<Col md={2} sm={7}>
+     
+  <div className="circle-wrap">
+  <div className="circle">
+    <div className="mask full-3">
+      <div className="fill-3"></div>
+    </div>
+    <div className="mask half">
+      <div class="fill-3">
+      </div>
+      
+    </div>
+   
+<div className="inside-circle3">
+<FontAwesomeIcon
                 icon={faJs}
                 style={{
                   color: "yellow",
-
-                  margin: "10",
+                  fontSize: "5rem",
+                  margin: "20",
                 }}
               />
-              <FontAwesomeIcon
+              <span className="progress-value">
+              <CountUp start={0}
+               end={30}
+               duration={4.75}></CountUp>%</span>
+</div>
+  </div>
+</div>
+        
+</Col>
+<Col md={2} sm={7}>
+
+  <div className="circle-wrap">
+  <div className="circle">
+    <div className="mask full-4">
+      <div className="fill-4"></div>
+    </div>
+    <div className="mask half">
+      <div class="fill-4">
+      </div>
+      
+    </div>
+<div className="inside-circle4">
+<FontAwesomeIcon
                 icon={faNodeJs}
                 style={{
                   color: "green",
-
-                  margin: "10",
+                  fontSize: "5rem",
+                  margin: "20",
                 }}
               />
-              <FontAwesomeIcon
+              <span className="progress-value">
+              <CountUp start={0} 
+              end={30}
+              duration={9}></CountUp>%</span>
+</div>
+  </div>
+</div>
+
+</Col>
+<Col md={2} sm={7}>
+
+  <div className="circle-wrap">
+  <div className="circle">
+    <div className="mask full-5">
+      <div className="fill-5"></div>
+    </div>
+    <div className="mask half">
+      <div class="fill-5">
+      </div>
+      
+    </div>
+<div className="inside-circle5">
+<FontAwesomeIcon
                 icon={faHtml5}
                 style={{
                   color: "orange",
-
-                  margin: "10",
+                  fontSize: "5rem",
+                  margin: "20",
                 }}
               />
-            </div>
+              <span className="progress-value">
+              <CountUp start={0} 
+              end={70}
+              duration={20}></CountUp>%</span>
+</div>
+  </div>
+</div>
+
+ </Col>
+ </Row>
+ </div>
           </Card>
-        </Col>
-      </Row>
+       
+     
     </Container>
   );
 }
@@ -83,8 +208,22 @@ const Card = styled(motion.div)`
   .skills {
     display: flex;
     position: relative;
-    top: 24rem;
+    top: 20rem;
+    text-align: center;
+    
+  }
+  h2{
+    color: orange;
+    margin: 1rem;
+  }
 
+  .progress-value{
+    margin-top: -20px;
+  }
+
+  .circle-container{
     place-content: center;
   }
+
+
 `;
